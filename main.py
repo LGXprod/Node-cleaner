36ccbc83441bae292ffd781990f8f9be5d617192
Matthew <mattghanno@gmail.com>
2020-02-07 00:38:11 +1100

    Began including edge cases where semicolons should not be concatenated

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 def addSemicolons(file):
+	textFile = ""
+
 	for line in file:
 		currentLine = list(line)
 		print(currentLine)
-		if currentLine[len(currentLine) - 1] != "{" or currentLine[len(currentLine) - 1] != "}":
-			print("good")
+
+		lastChar = ""
+
+		if len(currentLine) > 1:
+			lastChar = currentLine[(len(currentLine) - 2)]
+		else:
+			lastChar = currentLine[0]
+		
+		if lastChar != "{" and lastChar != "}" and lastChar != ";" and lastChar != "\n" + lastChar != "+":
+			for y in range(0, len(currentLine) - 1):
+				textFile += currentLine[y]
+			textFile += ";\n"
+		else:
+			textFile += line
+
+		firstChar = ""
+
+		for y in range(0, len(currentLine) - 1):
+			if currentLine[y] != " ":
+				firstChar = currentLine[y]
+				break
+
+		if firstChar == "+":
+			textFile = textFile[:-2] + "\n"
+			# print(textFile)
+
+	print(textFile)
 
 addSemicolons(open("test.js", "r"))
